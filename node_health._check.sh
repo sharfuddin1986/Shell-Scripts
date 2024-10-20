57d472f82fc20cb7f3160bda78b5852f7b5de87d
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-10-20 23:21:36 +0300

    Create node_health._check.sh

---
+++ node_health._check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################
+
+
+# Author: Sharfuddin
+# Date: 01/01/2024
+#
+# This script output the  node health
+# Version: V1
+#####################################
+
+set -x # debug mode
+
+df -h
+
+free -g
+
+nproc
+
